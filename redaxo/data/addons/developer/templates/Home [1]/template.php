522522c18ea601ddbf08cc3dd021ff5217f9638e
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-05-19 11:38:47 +0200

    Use jQuery 3.5. in frontend

--- redaxo/data/addons/developer/templates/Home [1]/template.php
+++ redaxo/data/addons/developer/templates/Home [1]/template.php
@@ -54,7 +54,7 @@
 
 </div>
 
-<script src="/js/jquery-3.3.1.js"></script>
+<script src="/js/jquery-3.5.1.js"></script>
 <script src="/js/popper.js"></script>
 <script src="/js/bootstrap.js"></script>
 
--- redaxo/data/addons/developer/templates/Standard [4]/template.php
+++ redaxo/data/addons/developer/templates/Standard [4]/template.php
@@ -66,7 +66,7 @@
 
 </div>
 
-<script src="/js/jquery-3.3.1.js"></script>
+<script src="/js/jquery-3.5.1.js"></script>
 <script src="/js/popper.js"></script>
 <script src="/js/bootstrap.js"></script>
     
