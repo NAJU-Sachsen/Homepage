f96cb089137e9089699dfadd28fae464c4163d4f
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-08-26 16:16:06 +0200

    Refactor Redaxo variables into valid PHP code

--- redaxo/data/addons/developer/modules/Kontakt-Informationen [4]/input.php
+++ redaxo/data/addons/developer/modules/Kontakt-Informationen [4]/input.php
@@ -1,14 +1,17 @@
 <?php
-
 $local_groups = rex_sql::factory()->setQuery('select group_id, group_name from naju_local_group')->getArray();
 
+$rex_var_group = "REX_VALUE[id=1 ifempty='']";
+$rex_var_heading = "REX_VALUE[id=2 ifempty='']";
 ?>
 
 <div class="form-group">
     <label for="select-local-group">Ortsgruppe auswählen</label>
     <select class="form-control" name="REX_INPUT_VALUE[1]">
         <?php foreach ($local_groups as $group) : ?>
-        <option value="<?= htmlspecialchars($group['group_id']); ?>" <?= REX_VALUE[id=1 ifempty=''] == $group['group_id'] ? 'selected' : '' ?>><?= htmlspecialchars($group['group_name']); ?></option>
+        <option value="<?= htmlspecialchars($group['group_id']); ?>" <?= $rex_var_group == $group['group_id'] ? 'selected' : '' ?>>
+          <?= htmlspecialchars($group['group_name']); ?>
+        </option>
         <?php endforeach; ?>
 
     </select>
@@ -17,7 +20,7 @@ $local_groups = rex_sql::factory()->setQuery('select group_id, group_name from n
     <div class="checkbox">
         <label>
             <input type="hidden" name="REX_INPUT[2]" value="false">
-            <input type="checkbox" name="REX_INPUT_VALUE[2]" value="true" <?= REX_VALUE[id=2 ifempty=''] == 'true' ? 'checked' : '' ?>>
+            <input type="checkbox" name="REX_INPUT_VALUE[2]" value="true" <?= $rex_var_heading == 'true' ? 'checked' : '' ?>>
             Überschrift über den Kontakt-Informationen anzeigen
         </label>
     </div>
