bf7b1d0eb3caf5fec54dd5ecc52ee4a8420e0fee
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-04 16:29:47 +0200

    Improve image module to support sizing and positioning

--- redaxo/data/addons/developer/modules/Foto [11]/input.php
+++ redaxo/data/addons/developer/modules/Foto [11]/input.php
@@ -1,4 +1,32 @@
+
+<div class="form-inline">
+    <div class="form-group">
+        <label for="image-width">Breite</label>
+        <div class="input-group">
+            <input type="number" min="-1" id="image-width" name="REX_INPUT_VALUE[1]" value="REX_VALUE[id=1]" class="form-control">
+            <span class="input-group-addon">px</span>
+        </div>
+    </div>
+    <div class="form-group" style="margin-left: 20px;">
+        <label for="image-height">Höhe</label>
+        <div class="input-group">
+            <input type="number" min="-1" id="image-height" name="REX_INPUT_VALUE[2]" value="REX_VALUE[id=2]" class="form-control">
+            <span class="input-group-addon">px</span>
+        </div>
+    </div>
+</div>
+
 <div class="form-group">
     <label>Bild auswählen</label>
-    REX_MEDIA[id=1 widget=1 preview=1 types=jpg,jpeg,png]
+    REX_MEDIA[id=1 widget=1 types=jpg,jpeg,png]
+</div>
+
+<div class="form-group">
+    <label for="image-align">Bild in den Textfluss integrieren?</label>
+    <select name="REX_INPUT_VALUE[3]" id="image-align" class="form-control">
+        <?php $img_integrate = 'REX_VALUE[id=3 ifempty=no-integrate]'; ?>
+        <option value="no-integrate" <?= $img_integrate == 'no-integrate' ? 'selected' : '' ?>>nicht integrieren</option>
+        <option value="integrate-left" <?= $img_integrate == 'integrate-left' ? 'selected' : '' ?>>links abbilden</option>
+        <option value="integrate-right" <?= $img_integrate == 'integrate-right' ? 'selected' : '' ?>>rechts abbilden</option>
+    </select>
 </div>
--- redaxo/data/addons/developer/modules/Foto [11]/output.php
+++ redaxo/data/addons/developer/modules/Foto [11]/output.php
@@ -1,3 +1,37 @@
 
 <!-- mod_img -->
-<img src="/media/REX_MEDIA[1]" class="img-fluid p-4">
+
+<?php
+$img_width = 'REX_VALUE[id=1 ifempty=-1]';
+$img_height = 'REX_VALUE[id=2 ifempty=-1]';
+$img_integrate = 'REX_VALUE[id=3 ifempty=no-integrate]';
+
+$img_class = 'img-fluid p-4 ';
+
+switch($img_integrate) {
+    case 'integrate-left':
+        $img_class .= ' float-left mr-2 ';
+        break;
+    case 'integrate-right':
+        $img_class .= ' float-right ml-2 ';
+        break;
+    case 'no-integrate':
+        // fall through
+    default:
+        $img_class .= ' d-block mx-auto ';
+        break;
+}
+
+$img_dimens = '';
+if ($img_width > 0) {
+    $img_dimens .= ' width="' . rex_escape($img_width) . '" ';
+}
+
+if ($img_height > 0) {
+    $img_dimens .= ' height="' . rex_escape($img_heigth) . '" ';
+}
+
+?>
+
+
+<img src="/media/REX_MEDIA[1]" class="<?= rex_escape($img_class); ?>" <?= $img_dimens; ?>>
