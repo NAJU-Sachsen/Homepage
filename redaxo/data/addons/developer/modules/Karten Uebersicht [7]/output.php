e446bf787f91f3858e8788d1412d1df93119cd6d
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-05 22:11:37 +0200

    Add `alt` attributes to images in overview cards

--- redaxo/data/addons/developer/modules/Karten Uebersicht [7]/output.php
+++ redaxo/data/addons/developer/modules/Karten Uebersicht [7]/output.php
@@ -11,9 +11,12 @@ $col_lg_width = $bs_cols / $n_cards;
 	<div class="row">
 		<section class="card-deck">
 			<article class="card">
-				<?php $card1 = naju_rex_var::toArray('REX_VALUE[1]'); ?>
+				<?php
+				$card1 = naju_rex_var::toArray('REX_VALUE[1]');
+				$img1 = new naju_image('REX_MEDIA[id=1 ifempty=default-card-image.jpg]');
+				?>
 
-				<img src="/media/REX_MEDIA[1]" class="card-img-top">
+				<img src="/media/<?= $img1->name(); ?>" alt="<?= rex_escape($img1->altText()); ?>" class="card-img-top">
 				<div class="card-body">
 					<h3 class="card-title"><?= rex_escape($card1['title']); ?></h3>
 					<p class="card-text"><?= rex_escape($card1['content']); ?></p>
@@ -22,9 +25,12 @@ $col_lg_width = $bs_cols / $n_cards;
 			</article>
 
 			<article class="card">
-				<?php $card2 = naju_rex_var::toArray('REX_VALUE[2]'); ?>
+				<?php
+				$card2 = naju_rex_var::toArray('REX_VALUE[2]');
+				$img2 = new naju_image('REX_MEDIA[id=2 ifempty=default-card-image.jpg]');
+				?>
 
-				<img src="/media/REX_MEDIA[2]" class="card-img-top">
+				<img src="/media/<?= $img2->name(); ?>" alt="<?= rex_escape($img2->altText()); ?>" class="card-img-top">
 				<div class="card-body">
 					<h3 class="card-title"><?= rex_escape($card2['title']); ?></h3>
 					<p class="card-text"><?= rex_escape($card2['content']); ?></p>
@@ -34,9 +40,12 @@ $col_lg_width = $bs_cols / $n_cards;
 
 			<?php if($n_cards >= 3) : ?>
 			<article class="card">
-				<?php $card3 = naju_rex_var::toArray('REX_VALUE[3]'); ?>
+				<?php
+				$card3 = naju_rex_var::toArray('REX_VALUE[3]');
+				$img3 = new naju_image('REX_MEDIA[id=3 ifempty=default-card-image.jpg]');
+				?>
 
-				<img src="/media/REX_MEDIA[3]" class="card-img-top">
+				<img src="/media/<?= $img3->name(); ?>" alt="<?= rex_escape($img3->altText()); ?>" class="card-img-top">
 				<div class="card-body">
 					<h3 class="card-title"><?= rex_escape($card3['title']); ?></h3>
 					<p class="card-text"><?= rex_escape($card3['content']); ?></p>
@@ -47,9 +56,12 @@ $col_lg_width = $bs_cols / $n_cards;
 
 			<?php if($n_cards >= 4) : ?>
 			<article class="card">
-				<?php $card4 = naju_rex_var::toArray('REX_VALUE[4]'); ?>
+				<?php
+				$card4 = naju_rex_var::toArray('REX_VALUE[4]');
+				$img4 = new naju_image('REX_MEDIA[id=4 ifempty=default-card-image.jpg]');
+				?>
 
-				<img src="/media/REX_MEDIA[4]" class="card-img-top">
+				<img src="/media/<?= $img4->name(); ?>" alt="<?= rex_escape($img4->altText()); ?>" class="card-img-top">
 				<div class="card-body">
 					<h3 class="card-title"><?= rex_escape($card4['title']); ?></h3>
 					<p class="card-text"><?= rex_escape($card4['content']); ?></p>
