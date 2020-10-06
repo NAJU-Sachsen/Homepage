8db170765407b104fad906d1a480a87515449a50
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-06 18:55:05 +0200

    Extend mod_separator to provide margins

--- redaxo/data/addons/developer/modules/Trennlinie [10]/input.php
+++ redaxo/data/addons/developer/modules/Trennlinie [10]/input.php
@@ -1,1 +1,22 @@
-<hr>
+
+<div class="form-group">
+    <label for="margin-top">Abstand nach oben</label>
+    <div class="input-group">
+        <input type="number" name="REX_INPUT_VALUE[1]" id="margin-top" value="REX_VALUE[1]" class="form-control">
+        <span class="input-group-addon">px</span>
+    </div>
+</div>
+<div class="form-group">
+    <label for="margin-btm">Abstand nach unten</label>
+    <div class="input-group">
+        <input type="number" name="REX_INPUT_VALUE[2]" id="margin-top" value="REX_VALUE[2]" class="form-control">
+        <span class="input-group-addon">px</span>
+    </div>
+</div>
+<div class="form-group">
+    <input type="hidden" name="REX_INPUT_VALUE[3]" value="false">
+    <label>
+        <input type="checkbox" name="REX_INPUT_VALUE[3]" value="true">
+        Trennlinie anzeigen?
+    </label>
+</div>
--- redaxo/data/addons/developer/modules/Trennlinie [10]/output.php
+++ redaxo/data/addons/developer/modules/Trennlinie [10]/output.php
@@ -1,4 +1,23 @@
 
 <!-- mod_separator -->
 
-<hr>
+<?php
+$margin_top = 'REX_VALUE[id=1 ifempty=0]';
+$margin_btm = 'REX_VALUE[id=2 ifempty=0]';
+$show_hr = 'REX_VALUE[3]' == 'true';
+
+$style = '';
+if ($margin_top > 0) {
+    $style .= ' margin-top: ' . rex_escape($margin_top) . 'px; ';
+}
+if ($margin_btm > 0) {
+    $style .= ' margin-bottom: ' . rex_escape($margin_btm) . 'px; ';
+}
+
+if ($show_hr) {
+    echo '<hr style="' . $style . '">';
+} else {
+    echo '<div style="' . $style . '"></div>';
+}
+
+?>
