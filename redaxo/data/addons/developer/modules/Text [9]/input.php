70cc5a17678baf275ef55f35ef6a7a6de8c57fb8
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-07 12:16:47 +0200

    Choose text editor features based on user permissions

--- redaxo/data/addons/developer/modules/Text [9]/input.php
+++ redaxo/data/addons/developer/modules/Text [9]/input.php
@@ -1,5 +1,12 @@
+
+<?php
+$full_redactor_profile = 'full';
+$reduced_redactor_profile = 'links-bold-italic-lists';
+$editor_type = rex::getUser()->isAdmin() ? $full_redactor_profile : $reduced_redactor_profile;
+?>
+
 <fieldset class="form-horizontal">
   <div class="form-group">
-      <textarea class="form-control redactorEditor2-links-bold-italic-lists" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>
+      <textarea class="form-control redactorEditor2-<?= $editor_type; ?>" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>
   </div>
 </fieldset>
