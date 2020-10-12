b6ac5eb7ec2cdc1e0cc67f36cad859d8c1055fae
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-12 08:52:52 +0200

    Support custom IDs on text blocks

--- redaxo/data/addons/developer/modules/Text [9]/input.php
+++ redaxo/data/addons/developer/modules/Text [9]/input.php
@@ -10,3 +10,11 @@ $editor_type = rex::getUser()->isAdmin() ? $full_redactor_profile : $reduced_red
       <textarea class="form-control redactorEditor2-<?= $editor_type; ?>" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>
   </div>
 </fieldset>
+
+<fieldset>
+  <legend>Weitere Optionen:</legend>
+  <div class="form-group">
+    <label for="text-id">Dem Text eine ID geben</label>
+    <input type="text" name="REX_INPUT_VALUE[2]" id="text-id" class="form-control" value="REX_VALUE[2]">
+  </div>
+</fieldset>
--- redaxo/data/addons/developer/modules/Text [9]/output.php
+++ redaxo/data/addons/developer/modules/Text [9]/output.php
@@ -1,4 +1,17 @@
 
 <!-- mod_editor -->
+<?php
+$block_id = 'REX_VALUE[2]';
+$start_tag = '<div %%ID%%>';
+$close_tag = '</div>';
 
-REX_VALUE[1 output=html]
+if ($block_id) {
+    $start_tag = str_replace('%%ID%%', 'id="' . rex_escape($block_id) . '"', $start_tag);
+} else {
+    $start_tag = str_replace('%%ID%%', '', $start_tag);
+}
+
+echo $start_tag;
+echo 'REX_VALUE[1 output=html]';
+echo $close_tag;
+?>
