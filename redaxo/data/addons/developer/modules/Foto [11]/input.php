08dbe60f497cde49eaa6b2b4f197b86a01dbcf04
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-12 10:35:07 +0200

    Improve image module ui

--- redaxo/data/addons/developer/modules/Foto [11]/input.php
+++ redaxo/data/addons/developer/modules/Foto [11]/input.php
@@ -1,54 +1,38 @@
 
-<div class="form-inline" style="margin-bottom: 20px;">
-    <div class="form-group">
-        <label for="image-width">Breite</label>
-        <div class="input-group">
-            <input type="number" min="-1" id="image-width" name="REX_INPUT_VALUE[1]" value="REX_VALUE[id=1]" class="form-control">
-            <span class="input-group-addon">px</span>
-        </div>
-    </div>
-    <div class="form-group" style="margin-left: 20px;">
-        <label for="image-height">Höhe</label>
-        <div class="input-group">
-            <input type="number" min="-1" id="image-height" name="REX_INPUT_VALUE[2]" value="REX_VALUE[id=2]" class="form-control">
-            <span class="input-group-addon">px</span>
-        </div>
-    </div>
-    <div class="form-group" style="margin-left: 20px;">
-        <label>
-            <input type="hidden" name="REX_INPUT_VALUE[4]" value="false">
-            <input type="checkbox" name="REX_INPUT_VALUE[4]" value="true" <?= "REX_VALUE[id=4 ifempty='']" == 'true' ? 'checked' : '' ?>>
-            tolle Bildeffekte aktivieren?
-        </label>
-    </div>
-</div>
+<?php
+$img_id = 1;
+$link_id = 1;
+$width_id = 1;
+$height_id = 2;
+$integrate_id = 3;
+$activate_effects_id = 4;
+$effects_kind_id = 5;
 
-<div class="form-group">
-    <label>Bild auswählen</label>
-    REX_MEDIA[id=1 widget=1 types=jpg,jpeg,png]
-</div>
+$effects_kind_select = ['random' => 'zufällig',
+    'img-fancy-default' => 'Standard (Dunkelrot)',
+    'img-fancy-green' => 'Maigrün (Hellgrün)',
+    'img-dark-green' => 'Laubgrün (Dunkelgrün)'];
+$integrate_select = ['no-integrate' => 'nicht integrieren',
+    'integrate-left' => 'links abbilden',
+    'integrate-right' => 'rechts abbilden'];
 
-<div class="form-group">
-    <label for="image-align">Bild in den Textfluss integrieren?</label>
-    <select name="REX_INPUT_VALUE[3]" id="image-align" class="form-control">
-        <?php $img_integrate = 'REX_VALUE[id=3 ifempty=no-integrate]'; ?>
-        <option value="no-integrate" <?= $img_integrate == 'no-integrate' ? 'selected' : '' ?>>nicht integrieren</option>
-        <option value="integrate-left" <?= $img_integrate == 'integrate-left' ? 'selected' : '' ?>>links abbilden</option>
-        <option value="integrate-right" <?= $img_integrate == 'integrate-right' ? 'selected' : '' ?>>rechts abbilden</option>
-    </select>
-</div>
+$mform = new MForm();
 
-<div class="form-group">
-    <label for="fancy-color">Effektfarbe für die tollen Bildeffekte</label>
-    <select name="REX_INPUT_VALUE[5]" class="form-control" id="fancy-color">
-        <option value="random">zufällig</option>
-        <option value="img-fancy-default">Standard (Dunkelrot)</option>
-        <option value="img-fancy-green">Maigrün (Hellgrün)</option>
-        <option value="img-fancy-green-alternate">Laubgrün (Dunkelgrün)</option>
-    </select>
-</div>
+$mform->addTab('Bild');
+$mform->addMediaField($img_id, ['preview' => '1', 'types' => 'jpg,jpeg,png', 'label' => 'Bild auswählen']);
+$mform->addLinkField($link_id, ['label' => 'Verlinkung hinzufügen?']);
+$mform->closeTab();
 
-<div class="form-group">
-        <label>Eine Verlinkung einfügen?</label>
-        REX_LINK[id=1 widget=1]
-</div>
+$mform->addTab('Bildeffekte');
+$mform->addHiddenField($activate_effects_id, 'false');
+$mform->addCheckboxField($activate_effects_id, ['true' => 'tolle Bildeffekte aktivieren'],  ['label' => 'Aktivieren?']);
+$mform->addSelectField($effects_kind_id, $effects_kind_select, ['label' => 'Effektfarbe:']);
+$mform->closeTab();
+
+$mform->addTab('Optionen');
+$mform->addInputField('number', $width_id, ['label' => 'Breite', 'min' => -1]);
+$mform->addInputField('number', $height_id, ['label' => 'Höhe', 'min' => -1]);
+$mform->addSelectField(3, $integrate_select, ['label' => 'Bild in den Textfluss integrieren?']);
+$mform->closeTab();
+
+echo $mform->show();
