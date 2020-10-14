95312cec2ca576eb59d7c037c9e198d490a0489c
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-14 17:47:44 +0200

    Create prototype of a "further reading" module

---
+++ redaxo/data/addons/developer/modules/Artikel Navigation [25]/input.php
@@ -0,0 +1,5 @@
+<?php
+
+$mform = new MForm();
+
+echo $mform->show();
---
+++ redaxo/data/addons/developer/modules/Artikel Navigation [25]/output.php
@@ -0,0 +1,21 @@
+
+<!-- mod_article_nav -->
+
+<?php
+
+$content = '<div class="row row-cols-1 row-cols-md-3">';
+$articles = rex_category::getCurrent()->getArticles(true);
+
+foreach ($articles as $article) {
+    if ($article->getId() == rex_article::getCurrentId()) {
+        continue;
+    }
+    $content .= '<div class="col mb-4"><div class="card"><div class="card-body">';
+    $content .= '<h4 class="card-title">' . rex_escape($article->getName()) . '</h4>';
+    $content .= '<a href="' . rex_getUrl($article->getId()) . '" class="article-link">Weiterlesen</a>';
+    $content .= '</div></div></div>';
+}
+
+$content .= '</div>';
+
+echo $content;
