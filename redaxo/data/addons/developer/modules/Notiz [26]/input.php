d41d2c98c6f18ccbda05b15780a304392eb9ee12
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-16 17:29:45 +0200

    Add module for backend notes

---
+++ redaxo/data/addons/developer/modules/Notiz [26]/input.php
@@ -0,0 +1,9 @@
+
+<div class="alert alert-info">
+    Die Notiz wird nur im Backend angezeigt. Auf der eigentlichen Webseite ist sie nicht sichtbar.
+</div>
+
+<div class="form-group">
+    <label for="note-text">Was möchtest du notieren?</label>
+    <textarea name="REX_INPUT_VALUE[1]" id="note-text" class="form-control">REX_VALUE[1]</textarea>
+</div>
---
+++ redaxo/data/addons/developer/modules/Notiz [26]/output.php
@@ -0,0 +1,6 @@
+
+<?php
+
+if (rex::isBackend()) {
+    echo '<div class="alert alert-info">REX_VALUE[1]</div>';
+}
