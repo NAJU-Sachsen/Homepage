67e589f3744099bc53bf1f69d7e08c45b9aaef57
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-16 18:46:02 +0200

    Make Iframe height editable

--- redaxo/data/addons/developer/modules/Iframe [20]/input.php
+++ redaxo/data/addons/developer/modules/Iframe [20]/input.php
@@ -3,3 +3,11 @@
     <label for="iframe-url">Adresse</label>
     <input type="url" name="REX_INPUT_VALUE[1]" id="iframe-url" class="form-control" autocomplete="off" value="REX_VALUE[1]">
 </div>
+
+<div class="form-inline">
+    <!-- REX_VALUE id=2 is available! -->
+    <div class="form-group">
+        <label for="iframe-height">Höhe (optional)</label>
+        <input type="number" name="REX_INPUT_VALUE[3]" value="REX_VALUE[3]" class="form-control">
+    </div>
+</div>
--- redaxo/data/addons/developer/modules/Iframe [20]/output.php
+++ redaxo/data/addons/developer/modules/Iframe [20]/output.php
@@ -1,6 +1,14 @@
 
 <!-- mod_iframe -->
 
+<?php
+$height = 'REX_VALUE[id=3 ifempty=0]';
+
+if ($height) {
+    $height = 'height: ' . $height . 'vh';
+}
+?>
+
 <div class="container-fluid">
-    <iframe src="REX_VALUE[1]" style="width: 100%; border: none;" height="1000"></iframe>
+    <iframe src="REX_VALUE[1]" style="width: 100%; border: none;" <?= $height; ?>></iframe>
 </div>
