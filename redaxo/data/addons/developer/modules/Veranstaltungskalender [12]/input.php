6583b4ccb944591850827a7f1df837be54915f41
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-18 21:14:27 +0200

    Add 'other' event type to module selections

--- redaxo/data/addons/developer/modules/Veranstaltungskalender [12]/input.php
+++ redaxo/data/addons/developer/modules/Veranstaltungskalender [12]/input.php
@@ -67,5 +67,6 @@ $local_groups = rex_sql::factory()->setQuery($group_query)->getArray();
         <option value="workshop" <?= 'REX_VALUE[6]' == 'workshop' ? 'selected' : ''; ?>>Workshop</option>
         <option value="work_assignment" <?= 'REX_VALUE[6]' == 'work_assignment' ? 'selected' : ''; ?>>Arbeitseinsatz</option>
         <option value="group_meeting" <?= 'REX_VALUE[6]' == 'group_meeting' ? 'selected' : ''; ?>>Aktiventreffen</option>
+        <option value="other" <?= 'REX_VALUE[3]' == 'other' ? 'selected' : ''; ?>>Sonstige Veranstaltungen</option>
     </select>
 </div>
