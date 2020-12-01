83236feed13409cf271faa4962c7acadc2f9c673
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-12-01 20:59:10 +0100

    Display local groups on blog select

--- redaxo/data/addons/developer/modules/[Blog] Teaser [29]/input.php
+++ redaxo/data/addons/developer/modules/[Blog] Teaser [29]/input.php
@@ -15,7 +15,7 @@ $mform->addCheckboxField($id_show_newest, ['true' => 'Automatisch die neuesten A
 $mform->addSelectField($id_blog_select);
 $mform->setLabel('Blog auswählen');
 if (rex::getUser()->isAdmin()) {
-    $query = 'SELECT blog_title as name, blog_id as id FROM naju_blog ORDER BY blog_title';
+    $query = 'SELECT CONCAT(blog_title, " (", group_name, ")") as name, blog_id as id FROM naju_blog JOIN naju_local_group ON blog_group = group_id ORDER BY blog_title';
 } else {
     $user_id = rex::getUser()->getId();
     $query = 'SELECT blog_title as name, blog_id as id FROM naju_blog JOIN naju_group_account ON blog_group = group_id
