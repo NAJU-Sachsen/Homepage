ecb0f1a7fef423f9245fd468359c7193e1483930
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2022-04-07 12:53:01 +0200

    Make iframes load on-demand rather than by default
    This prevents issues with GDRP rules regarding transmission of
    "sensitive" data (i.e. IP addresses, cookies, etc.)

--- js/site.js
+++ js/site.js
@@ -1,14 +1,18 @@
 
 /**
  * site.js
- * v0.1.0
- * 
+ * v0.2.0
+ *
  */
+
+//
+// Listing cards height
+//
 
 function adjustListingCardsHeight(container) {
 	const cards = container.getElementsByClassName('listing-card');
 	const maxHeight = Math.max(...Array.from(cards).map((card) => card.clientHeight));
-	
+
 	for (const card of cards) {
 		card.style.height = '' + maxHeight + 'px';
 		const footers = card.getElementsByClassName('card-footer');
@@ -31,6 +35,34 @@ window.addEventListener('load', () => {
 		spyer.dataset.offset = window.innerHeight - spyer.getBoundingClientRect().top;
 		console.log(spyer);
 	}
-	$('[data-spy="scroll"]').each(() => $(this).scrollspy('refresh'));
+	//$('[data-spy="scroll"]').each(() => $(this).scrollspy('refresh'));
 
+});
+
+//
+// Iframe lazy loading
+//
+
+function lazyLoadIframe(container, src, height) {
+	const iframe = container.getElementsByClassName('iframe-holder')[0];
+	iframe.setAttribute('src', src);
+	iframe.style.height = height;
+	while (container.firstChild !== iframe) {
+		container.removeChild(container.firstChild);
+	}
+	while (container.lastChild !== iframe) {
+		container.removeChild(container.lastChild);
+	}
+	container.className = 'iframe-container loaded';
+	iframe.className = 'iframe-holder';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const iframeContainers = document.getElementsByClassName('iframe-container load-on-demand');
+	for (const iframe of iframeContainers) {
+		const src = iframe.dataset.iframeSrc;
+		const height = iframe.dataset.iframeHeight;
+		const loadBtn = iframe.getElementsByClassName('iframe-load-btn')[0];
+		loadBtn.addEventListener('click', () => lazyLoadIframe(iframe, src, height));
+	}
 });
--- redaxo/data/addons/developer/modules/Iframe [20]/output.php
+++ redaxo/data/addons/developer/modules/Iframe [20]/output.php
@@ -5,10 +5,17 @@
 $height = 'REX_VALUE[id=3 ifempty=0]';
 
 if ($height) {
-    $height = 'height: ' . $height . 'vh';
+    $height = $height . 'vh';
 }
 ?>
 
 <div class="container-fluid">
-    <iframe src="REX_VALUE[1]" style="width: 100%; border: none;" <?= $height; ?>></iframe>
+    <article class="iframe-container load-on-demand" data-iframe-src="REX_VALUE[1]" data-iframe-height="<?= $height; ?>">
+        <p class="iframe-load-description">
+            Hier soll Inhalt aus einer externen Quelle angezeigt werden.
+            Dabei werden Daten an den jeweiligen Anbieter übertragen.
+        </p>
+        <button type="button" class="iframe-load-btn">Jetzt laden</button>
+        <iframe src="" class="iframe-holder d-none"></iframe>
+    </article>
 </div>
