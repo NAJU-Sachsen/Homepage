535b4af34740663c65d21b98cf5a843a6c39515f
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2024-01-31 19:26:56 +0100

    Nicer hover animation for home navigation module

--- redaxo/data/addons/developer/modules/Home Navigation [33]/output.php
+++ redaxo/data/addons/developer/modules/Home Navigation [33]/output.php
@@ -5,12 +5,17 @@
         <nav id="quick-nav" class="col-lg-8 offset-lg-2 col-md-12">
             <ul class="nav-primary">
 
-                <?php foreach (explode(',', 'REX_LINKLIST[id=1]') as $article_id) : ?>
+                <?php foreach (explode(',', 'REX_LINKLIST[id=1]') as $article_id) :
+                        $article = rex_article::get($article_id);
+                        $article_url = $article->getUrl();
+                        $article_name = rex_escape($article->getName());
+                ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= rex_getUrl($article_id); ?>">
-                            <?= rex_article::get($article_id)->getName(); ?>
+                        <a class="nav-link" href="<?= $article_url ?>" title="<?= $article_name ?>">
+                            <?= $article_name ?>
                         </a>
                     </li>
+
                 <?php endforeach; ?>
 
             </ul>
